6082ccd0a90f53f7d568d0793b2b4bee5007de6d
Xar0P <pietroricardocres@hotmail.com>
2022-05-30 11:29:24 -0300

    handleClick to copy button

--- src/components/UploadSuccess/UploadSuccess.tsx
+++ src/components/UploadSuccess/UploadSuccess.tsx
@@ -10,18 +10,24 @@ import {
   CopyButton,
 } from './UploadSuccess.styles';
 
-const UploadSuccess: React.FC<{ url: string }> = ({ url }) => (
-  <Container>
-    <Icon className='material-icons'>check_circle</Icon>
-    <MessageSuccess>Upload Successfully!</MessageSuccess>
-    <Img src={url} />
-    <LinkImg>
-      <Url>{url}</Url>
-      <CopyButton>
-        <p>Copy Link</p>
-      </CopyButton>
-    </LinkImg>
-  </Container>
-);
+const UploadSuccess: React.FC<{ url: string }> = ({ url }) => {
+  const handleClick = async () => {
+    await navigator.clipboard.writeText(url);
+  };
+
+  return (
+    <Container>
+      <Icon className='material-icons'>check_circle</Icon>
+      <MessageSuccess>Upload Successfully!</MessageSuccess>
+      <Img src={url} />
+      <LinkImg>
+        <Url>{url}</Url>
+        <CopyButton onClick={handleClick}>
+          <p>Copy Link</p>
+        </CopyButton>
+      </LinkImg>
+    </Container>
+  );
+};
 
 export default UploadSuccess;
